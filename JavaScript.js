15d1dcd7fc6d39fc46d7c098c5e5df9428342474
unknown <sozisobrennolotier99@gmail.com>
2026-04-21 12:30:26 -0300

    Foi resolvido erro no código e aplicado a indentação

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,10 @@
 let senhaCadastrada = prompt("Cadastre sua senha:");
 let senhaConfirmacao = prompt("Digite novamente a senha para confirmação:");
 
-//O === garante que nada "parecido" seja aceito, apenas o que for exatamente igual.
+    //O === garante que nada "parecido" seja aceito, apenas o que for exatamente igual.
 if (senhaCadastrada === senhaConfirmacao) {
     alert("Acesso permitido.");
-} else if (senhaCadastrada !== senhaConfirmacao) {
+} else {
+    // Se não corresponde ao if, obrigatoriamente as senhas são diferentes.
     alert("Senha incorreta.");
 }
